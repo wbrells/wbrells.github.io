da931d89ea3ffd60af633cc2e5ddb42135466dc6
wbrells <44944548+wbrells@users.noreply.github.com>
2021-07-08 09:19:27 -0400

    Update password.js

--- password.js
+++ password.js
@@ -1,11 +1,24 @@
 var remaining = 3;
+
+function stringToHash(string) {
+	var hash = 0;
+    if (string.length == 0) return hash;
+    for (i = 0; i < string.length; i++) {
+		char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+	}
+	return hash;
+}
+
 function check(formId){
 	// Get the input value
 	var inputPw;
 	var inputPw = formId.textin.value;
-	// Set result
-	if (inputPw == 5280){
-		result = "Good PIN";
+	// Using hashing so actual PIN is not visible.
+	if (stringToHash(inputPw) == 1628757){
+		// result = stringToHash(inputPw);
+		result = "Good PIN!"
 		window.location.href = 'realindex.html'; // Run the real website.
 	}
 	else {
@@ -13,9 +26,8 @@ function check(formId){
 		if (remaining <= 0) {window.close();}
 		result = "Bad PIN. You can try " + remaining + " more times.";
 	}
-
 	
-	// Display result
+	// Display the "Bad PIN" message
 	
 	document.getElementById("textout").innerHTML = result;
 	
